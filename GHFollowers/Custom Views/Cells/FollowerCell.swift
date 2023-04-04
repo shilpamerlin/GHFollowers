4043c68186de110d6df664a52060b61e4d94e230
Shilpa Merlin joy <shilpamjoy@gmail.com>
2023-04-04 14:01:31 -0400

    Implemented pagination

--- GHFollowers/Custom Views/Cells/FollowerCell.swift
+++ GHFollowers/Custom Views/Cells/FollowerCell.swift
@@ -24,7 +24,7 @@ class FollowerCell: UICollectionViewCell {
     
     func set(follower: Follower) {
         usernameLabel.text = follower.login
-       // avatarImageView = follower.
+        avatarImageView.downloadImage(from: follower.avatarUrl)
     }
     
     private func configure() {
--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Shilpa Joy on 2023-03-27.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
