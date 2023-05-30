00e5bb3f4230d5be479d942b8ce8775a46160bf8
Shilpa Merlin joy <shilpamjoy@gmail.com>
2023-05-30 15:49:41 -0400

    Implemented userinfo vc

---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,21 @@
+//
+//  GFRepoItemVC.swift
+//  GHFollowers
+//
+//  Created by Shilpa Joy on 2023-05-26.
+//
+
+import Foundation
+
+class GFRepoItemVC: GFItemInfoVC {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}
--- GHFollowers/Utilities/Constants.swift
+++ GHFollowers/Utilities/Constants.swift
@@ -9,4 +9,8 @@ import Foundation
 
 enum SFSymbols {
     static let location = "mappin.and.ellipse"
+    static let repos = "folder"
+    static let gits = "text.alignleft"
+    static let followers = "heart"
+    static let following = "person.2"
 }
